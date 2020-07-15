47f7e78ebf335cbaa110add5fa222822b19d67d3
carlosworld <carlosworld93@gmail.com>
2020-07-15 18:24:50 -0500

    nesesario

--- db.php
+++
@@ -1,9 +0,0 @@
- 
-<?php 
-function getConn(){
-  $mysqli = mysqli_connect('localhost', 'root', '', 'test');
-  if (mysqli_connect_errno($mysqli))
-    echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
-  $mysqli->set_charset('utf8');
-  return $mysqli;
-}
